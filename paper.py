2aa17e192c73e3e06363ebc4a99ae9458995105d
satoken912 <seat.soil.74196901@gmail.com>
2018-10-07 01:59:07 +0900

    get titetitle and authors from all pages

--- paper.py
+++ paper.py
@@ -1,7 +1,26 @@
 import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 
-html = urllib.request.urlopen("http://dl.nkmr-lab.org/papers/1")
+for i in range(1,107):
+    try:
+        html = urllib.request.urlopen("http://dl.nkmr-lab.org/papers/"+str(i))
+    except urllib.error.HTTPError as e:
+        continue
 
-soup = BeautifulSoup(html)
-print( soup )
+    soup = BeautifulSoup(html)
+    print( soup )
+
+    title = soup.find("h1").get_text()
+    print( title )
+    print()
+
+    div = soup.find("div")
+    div2 = div.find_all("div")[1]
+    div3 = div2.find("div") # _2cXX...
+    div4 = div3.find_all("div", recursive=False)[0]
+    div5 = div4.find("div")
+    header = div5.find("header")
+
+    for a in header.find_all("a"):
+        author = a.get_text(" ", strip=True)
+        print(author)
